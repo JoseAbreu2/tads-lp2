f188556bdad02c54d5c99a30aa607feb06ea752b
Adailton Abreu <adailton.abreu@aluno.ifsp.edu.br>
2026-09-03 21:52:57 -0300

    Aula-03-09

--- src/main/java/br/edu/ifsp/biblioteca/BibliotecaApplication.java
+++ src/main/java/br/edu/ifsp/biblioteca/BibliotecaApplication.java
@@ -2,34 +2,63 @@ package br.edu.ifsp.biblioteca;
 
 
 import br.edu.ifsp.biblioteca.domain.Livro;
+import br.edu.ifsp.biblioteca.repository.ILivroRepository;
+import br.edu.ifsp.biblioteca.repository.LivroRepositoryEmMemoria;
+
+import java.util.List;
+import java.util.Optional;
 
 public class BibliotecaApplication {
 
     public static void main(String[] args) {
 
         Livro livro = new Livro(
-                1L,
                 "9788508145607",
                 "Dom Casmurro",
                 2026
         );
 
         Livro livro2 = new Livro(
-                2L,
                 "98989",
                 "Dom Quixote",
                 2025
         );
 
         Livro livro3 = new Livro(
-                3L,
                 "9898555559",
                 "Crime e Castigo",
                 1913
         );
 
-        System.out.println(livro);
-        System.out.println(livro2);
-        System.out.println(livro3);
+        ILivroRepository livroRepository = new LivroRepositoryEmMemoria();
+        livroRepository.salvar(livro);
+        livroRepository.salvar(livro);
+        livroRepository.salvar(livro2);
+        livroRepository.salvar(livro3);
+
+        Optional<Livro> livro1Optional = livroRepository.buscarPorId(1L);
+        Optional<Livro> livro2Optional = livroRepository.buscarPorId(10L);
+
+        if (livro1Optional.isPresent()){
+            Livro l1 = livro1Optional.get();
+
+            System.out.println("Livro com ID 1 encontrado: " + l1);
+        }
+
+        if (livro2Optional.isPresent()){
+            Livro l2 = livro2Optional.get();
+
+            System.out.println("Livro com ID 1 encontrado: " + l2);
+        } else {
+            System.out.println("Livro com ID 10 não encontrado!");
+        }
+
+        List<Livro> encontrados = livroRepository.buscarPorTitulo("Casmurro");
+
+        System.out.println(encontrados);
+
+        //System.out.println(livro);
+        //System.out.println(livro2);
+        //System.out.println(livro3);
     }
 }
